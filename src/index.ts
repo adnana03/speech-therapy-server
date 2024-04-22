9da899079b30cd2b43a8849f2e579064649975f2
adnana03 <801567@unizar.es>
2024-04-22 14:03:53 +0200

    self signed certs

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,21 @@
-const express = require("express");
-const cors = require("cors");
-const Stream = require("node-rtsp-stream");
+import express from "express";
+import https from "https";
+import fs from "fs";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const options: https.ServerOptions = {
+  key: fs.readFileSync("~/self-signed-cert/server.key"),
+  cert: fs.readFileSync("~/self-signed-cert/server.crt"),
+};
 
-const port = 3000;
-
-let stream: any = null;
-
-app.post("/start", function (req: any, res: any) {
-  if (stream !== null) {
-    stream.stop();
-    stream = null;
-  }
-
-  try {
-    stream = new Stream({
-      name: "videoStream",
-      streamUrl: "rtsp://192.168.1.110:8080/h264_ulaw.sdp",
-      wsPort: 9999,
-      ffmpegOptions: {
-        "-stats": "",
-        "-r": 30,
-      },
-    });
-
-    res.json({ url: `ws://127.0.0.1:${9999}` });
-  } catch (e) {
-    console.error(e);
-  }
+const server = https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end("Hello, HTTPS World!");
 });
 
-app.listen(port, () => {
-  console.log(
-    `server listening commands at http://127.0.0.1:${port} and serve stream at ws://127.0.0.1:${9999}`
-  );
+const PORT = 443;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
