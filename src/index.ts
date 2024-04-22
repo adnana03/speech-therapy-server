a3e903fbc78b3e824af01d11177233a8a83e3073
adnana03 <801567@unizar.es>
2024-04-22 13:39:53 +0200

    rtsp

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,41 @@
-/*
-import express, { Request, Response } from "express";
-import cors from "cors";
-import taskRoutes from "./shared/routes/tasks";
-import camaraRoutes from "./shared/routes/camaraIP";
+const express = require("express");
+const cors = require("cors");
+const Stream = require("node-rtsp-stream");
 
 const app = express();
-const port = 3001;
 
 app.use(cors());
-app.use(express.static("public"));
-
 app.use(express.json());
-app.use("/tasks", taskRoutes);
-app.use("/camaraIP", camaraRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-  const data = { message: "¡Hola desde el servidor Express!" };
-  res.json(data);
+const port = 3000;
+
+let stream: any = null;
+
+app.post("/start", function (req: any, res: any) {
+  if (stream !== null) {
+    stream.stop();
+    stream = null;
+  }
+
+  try {
+    stream = new Stream({
+      name: "videoStream",
+      streamUrl: "rtsp://192.168.1.110:8080/h264_ulaw.sdp",
+      wsPort: 9999,
+      ffmpegOptions: {
+        "-stats": "",
+        "-r": 30,
+      },
+    });
+
+    res.json({ url: `ws://127.0.0.1:${9999}` });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`El servidor está corriendo en el puerto ${port}`);
-});
-
-*/
-
-import express from "express";
-import http from "http";
-import { Server, Socket } from "socket.io";
-import useIPCamera from "./shared/hooks/useIPCamera";
-
-const app = express();
-const httpServer = http.createServer(app);
-
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
-
-const { handleSingleCapture } = useIPCamera();
-
-io.on("connection", (socket: Socket) => {
-  console.log("Cliente conectado");
-
-  socket.on("tomarFoto", async () => {
-    console.log("Solicitud para tomar la foto recibida");
-
-    const imgURL = await handleSingleCapture();
-
-    console.log("Enviando url image...");
-
-    if (imgURL) {
-      socket.emit("fotoLista", imgURL);
-    } else {
-      socket.emit("fotoLista", "No se ha podido obtener la imagen");
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Cliente desconectado");
-  });
-});
-
-const PORT = 3001;
-httpServer.listen(PORT, () => {
-  console.log(`Servidor WebSocket escuchando en el puerto ${PORT}`);
+  console.log(
+    `server listening commands at http://127.0.0.1:${port} and serve stream at ws://127.0.0.1:${9999}`
+  );
 });
