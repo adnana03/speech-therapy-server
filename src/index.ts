232c9fd2d2173832ff94137f1ff54611d750ec13
adnana03 <801567@unizar.es>
2024-04-22 09:02:07 +0200

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+
 const app = express();
 const port = 3001; // o cualquier otro puerto que prefieras
+
+app.use(cors());
 
 app.get("/api/data", (req: Request, res: Response) => {
   // Maneja tu lógica de la API aquí
