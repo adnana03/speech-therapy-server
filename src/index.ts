be373f700cce25c45dc68f59baecf75851db93ea
adnana03 <801567@unizar.es>
2024-04-22 11:58:31 +0200

    added cors

--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,13 @@ app.listen(port, () => {
 import express from "express";
 import http from "http";
 import { Server, Socket } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(cors());
 
 io.on("connection", (socket: Socket) => {
   console.log("Cliente conectado");
