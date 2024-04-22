070cd92c8374d9b386ca71e5e663ee018f72d9ff
adnana03 <801567@unizar.es>
2024-04-22 11:00:38 +0200

    added single image

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import taskRoutes from "./shared/routes/tasks";
+import camaraRoutes from "./shared/routes/camaraIP";
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use("/tasks", taskRoutes);
+app.use("/camaraIP", camaraRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  // Maneja tu lógica de la API aquí
   const data = { message: "¡Hola desde el servidor Express!" };
   res.json(data);
 });
---
+++ src/shared/routes/camaraIP.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from "express";
+import useIPCamera from "../hooks/useIPCamera";
+
+const router = Router();
+
+const { handleSingleCapture } = useIPCamera();
+
+// Read single image
+router.get("/", async (req: Request, res: Response) => {
+  const imgURL = await handleSingleCapture();
+
+  console.log("URL image => " + imgURL);
+  res.sendFile(imgURL);
+});
+
+export default router;
