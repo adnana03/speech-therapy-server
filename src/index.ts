6f009d56340cf8153dc315ea993b6d0886a0f7f5
adnana03 <801567@unizar.es>
2024-04-24 12:19:40 +0200

    added limit for requests up to 50 MB

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ app.use(
 );
 app.use(express.static("public"));
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use("/webcam", webcamRoutes);
 app.use("/tasks", taskRoutes);
 
